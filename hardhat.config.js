9eb7dcaa52d6f7b5af87e13c65c423855d073ec4
Rohit Tandon <rohitandon24@gmail.com>
2022-08-15 19:33:08 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,13 +13,13 @@ require("hardhat-deploy")
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
     process.env.KOVAN_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+    "https://eth-mainnet.alchemyapi.io/v2/qQgbd7wZWgtR0diYQLPP5VptZIH-7gbE"
 const RINKEBY_RPC_URL =
     process.env.RINKEBY_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+    "https://eth-mainnet.alchemyapi.io/v2/qQgbd7wZWgtR0diYQLPP5VptZIH-7gbE"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
